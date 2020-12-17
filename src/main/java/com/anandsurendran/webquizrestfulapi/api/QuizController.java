c48209524d321ce31c9d912787433a4017f24fa6
Anand <anandsnair2005@gmail.com>
2020-12-18 00:34:10 +0530

    Modified API to add quiz questions

--- src/main/java/com/anandsurendran/webquizrestfulapi/api/QuizController.java
+++ src/main/java/com/anandsurendran/webquizrestfulapi/api/QuizController.java
@@ -4,16 +4,25 @@ import com.anandsurendran.webquizrestfulapi.entity.AnswerResponse;
 import com.anandsurendran.webquizrestfulapi.entity.QuizQuestion;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
 @RequestMapping("/api")
 public class QuizController {
 
+    ArrayList<QuizQuestion> quizQuestions = new ArrayList<>();
+    private static AtomicInteger id = new AtomicInteger();
+
     @GetMapping(path = "/quiz")
     public QuizQuestion getQuiz() {
         QuizQuestion defaultQuestion = new QuizQuestion(
+                id.getAndIncrement(),
                 "The Java Logo",
                 "What is depicted on the Java logo?",
-                new String[]{"Robot", "Tea leaf", "Cup of coffee", "Bug"}
+                new String[]{"Robot", "Tea leaf", "Cup of coffee", "Bug"},
+                2
         );
         return defaultQuestion;
     }
@@ -29,5 +38,16 @@ public class QuizController {
             response.setFeedback("Wrong answer! Please, try again.");
         }
         return response;
+    }
+
+    @PostMapping(path = "/quizzes")
+    public QuizQuestion addQuestion(@RequestBody QuizQuestion inputQuestion) {
+        QuizQuestion inputQuestionWithID = new QuizQuestion(id.getAndIncrement(),
+                inputQuestion.getTitle(),
+                inputQuestion.getText(),
+                inputQuestion.getOptions(),
+                inputQuestion.getAnswer());
+        quizQuestions.add(inputQuestionWithID);
+        return quizQuestions.get(quizQuestions.size()-1);
     }
 }
