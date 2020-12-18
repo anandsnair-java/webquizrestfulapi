71a6b5b64694bc6caa2e37a03b9df2f6b13c8443
Anand <anandsnair2005@gmail.com>
2020-12-18 21:56:15 +0530

    Added answer as an array for MCQ quiz

--- src/main/java/com/anandsurendran/webquizrestfulapi/entity/QuizQuestion.java
+++ src/main/java/com/anandsurendran/webquizrestfulapi/entity/QuizQuestion.java
@@ -3,18 +3,19 @@ package com.anandsurendran.webquizrestfulapi.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @ToString @AllArgsConstructor @NoArgsConstructor
 public class QuizQuestion {
     @Getter @Setter
     private int id;
-    @Getter @Setter
+    @Getter @Setter @NotEmpty
     private String title;
-    @Getter @Setter
+    @Getter @Setter @NotEmpty
     private String text;
-    @Getter @Setter
+    @Getter @Setter @NotEmpty @Size(min = 2)
     private String[] options;
     @Getter @Setter @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private int answer;
+    private int[] answer;
 }
